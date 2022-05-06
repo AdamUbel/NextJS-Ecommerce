5b018acff56ed37efd16125cbe30fa23c5172a98
Adam <fallenempire.tv@gmail.com>
2022-05-06 15:48:00 -0700

    modified for build

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { client } from "../lib/client";
-import { HeroBanner, FooterBanner, Product } from "../components";
+import { HeroBanner, FooterBanner, Product } from "../components/Index";
 
 const Home = ({ products, bannerData }) => {
   return (
